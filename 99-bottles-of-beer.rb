9f32c875e620332d503b069946a9d7fed6442f05
awanderson8 <awanderson8@gmail.com>
2014-03-14 15:19:03 -0500

    Added 99-bottles-of-beer.rb

---
+++ 99-bottles-of-beer.rb
@@ -0,0 +1,9 @@
+num = 99
+while num > 0 
+puts "#{num} bottles of beer on the wall"
+puts "#{num} bottles of beer!"
+puts "Take one down, pass it around"
+num = num - 1
+puts "#{num} bottles of beer on the wall!"
+puts ""
+end
